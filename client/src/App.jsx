701f4fec66156427e66cd4f121de03b3bd16552e
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2022-02-03 23:57:05 +0530

    refractored frontend code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-// Import the required libraries and components
 import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from "./components";
-
 import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-// our entire react application is going to inside the "App" component
-// and we are going to hook that component into the root div
+// our entire react app is inside the "App" component
+// and we are hook-ing that component into the root div
 // the root div is inside the ./public/index.html file
 ReactDOM.render(<App />, document.getElementById('root'));
